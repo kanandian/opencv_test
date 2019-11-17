f4bcce41cf8728a934c36f654279187ddf703595
kanandian <kanandian@gmail.com>
2019-11-17 13:04:19 +0800

    init

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
-screen_width = 800
-screen_height = 500
+screen_width = 1280
+screen_height = 800
 background_color = (111, 111, 111)
 leaf_width = 80
 leaf_height = 50
--- utils.py
+++ utils.py
@@ -7,14 +7,17 @@ def cv_show2(name, image):
     #cv2.waitKey(1000) #显示1000毫秒
     cv2.destroyAllWindows()
 
+
 def cv_show1(image):
     cv_show2("image", image)
+
 
 def cv_show(*args):
     if len(args) == 1:
         cv_show1(args[0])
     elif len(args) == 2:
         cv_show2(args[0]. args[1])
+
 
 def is_out_of_range(leaf):
     if (leaf.position_y+constant.leaf_height<0 or leaf.position_y>constant.screen_height or leaf.position_x+constant.leaf_width<0 or leaf.position_x>constant.screen_width):
